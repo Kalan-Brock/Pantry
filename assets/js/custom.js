b6cda4005ddeef0f852f10d04f13cecbf09c2f18
Kalan Brock <kalan@practicecafe.com>
2019-01-29 19:29:42 -0500

    Nav items get marked as active dynamically now.

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,1 +1,20 @@
 console.log('Powered by pantry.');
+
+jQuery(document).ready( function($) {
+    var path = window.location.pathname;
+    var activenav;
+
+    switch(path) {
+        case '/':
+            activenav = 'home';
+            break;
+        case '/example':
+            activenav = 'example';
+            break;
+        case '/blog':
+            activenav = 'blog';
+            break;
+    }
+
+    $('li.nav-item.' + activenav).addClass('active');
+});
