110acd0f47fe90cfe417d4567e4295dc1d5cb9bc
Kalan Brock <kalan@practicecafe.com>
2019-01-29 21:44:40 -0500

    Versioned from Pantry CMS.

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ const config = {
     siteUrl: "http://localhost:5000",
     sitePort: 5000,
     siteName: "Pantry",
-    siteTitle: "The Front End Developer CMS",
+    siteTitle: "A Javascript Web Framework",
     hasBlog: true,
     tinymceApiKey: "smvdz7llfos6ndawowwgg0goeq08p1kakizrc91ncpbspn09",
     sparkPostApiKey: "",
