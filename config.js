81800c6a3dd04299115507a94f2bd0e650c7e9d0
Kalan Brock <kalan@practicecafe.com>
2019-01-30 11:42:02 -0500

    Changing some wording, it's not really a CMS afterall.

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ const config = {
     siteUrl: "http://localhost:5000",
     sitePort: 5000,
     siteName: "Pantry",
-    siteTitle: "A Javascript Web Framework",
+    siteTitle: "A Web Development Framework",
     hasBlog: true,
     tinymceApiKey: "smvdz7llfos6ndawowwgg0goeq08p1kakizrc91ncpbspn09",
     sparkPostApiKey: "",
