2ef27fa01abbe477184d705d58a59e76446b553e
Kalan Brock <kalan@practicecafe.com>
2019-01-31 18:52:21 -0500

    Accidentally left a console log in there.

--- tools/gitpush.js
+++ tools/gitpush.js
@@ -2,5 +2,5 @@ const config = require('../config/config.js');
 const simpleGit = require('simple-git')('./');
 
 if(global.gConfig.gitAutoVersion) {
-    simpleGit.add('.').commit('Versioned from Pantry CMS.').push('origin', 'master');
+    simpleGit.add('.').commit('Auto pushed from Pantry.').push('origin', 'master');
 }
--- tools/staticpages.js
+++ tools/staticpages.js
@@ -1,5 +1,4 @@
 const config = require('../config/config.js');
-console.log(global.gConfig);
 const low = require('lowdb');
 const FileSync = require('lowdb/adapters/FileSync');
 const adapter = new FileSync('db.json');
