2231479c39c5a26b627977f19a18e21abfda8efa
Kalan Brock <kalan@practicecafe.com>
2019-02-01 11:08:02 -0500

    Some lorem ipsum for the example page when db is initialized.

--- setup/dbinit.js
+++ setup/dbinit.js
@@ -15,7 +15,7 @@ db.defaults({
             "slug": "example",
             "layout": "page",
             "title": "Example Page",
-            "content": "<div class=\"container\"><div class=\"row\"><div class=\"col-12\"><h1>Example Page</h1></div></div></div>",
+            "content": "<div class=\"container\"><div class=\"row\"><div class=\"col-12\"><h1>Example Page</h1><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p></div></div></div>",
             "author": "Administrator",
             "published": true,
             "should_cache": true,
