91904f8377798e5e3e737f68936012c343ce2d6f
Kalan Brock <kalan@practicecafe.com>
2019-02-01 15:22:03 -0500

    Fixed a detach issue.

--- tools/webserver.js
+++ tools/webserver.js
@@ -1,5 +1,5 @@
 const config = require('../config/config.js');
 const exec = require('child_process').exec;
 
-exec('pm2 delete ' + global.gConfig.appName, { detached: true, windowsHide: true });
-exec('pm2 start app.js --name "' + global.gConfig.appName +'" --watch', { detached: true, windowsHide: true });
+exec('pm2 delete ' + global.gConfig.appName, { windowsHide: true });
+exec('pm2 start app.js --name "' + global.gConfig.appName +'" --watch', { windowsHide: true });
